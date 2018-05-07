e1007e45618f143f54b748198e16c1be53ea1e55
sjillen <thomas.proust89@gmail.com>
2018-05-07 12:10:56 +0900

    add customs nested routes for campaign show, add functions to campaign contract, redeployed campaignFactory and instance of campaign created to access variables from getSummary()

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(JSON.parse(
     CampaignFactory.interface),
-    '0xb57a22E109381A87E5CF79cea639F4B60E1eFc49'
+    '0x41273577b1d8f6b5f8804B48fF234D38c388e64E'
     );
 
 export default instance;
